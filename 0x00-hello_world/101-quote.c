ac0e856d41486fdfb3a35753355ea6c7e1fb4228
SimplicityND <ronaldomeh@yahoo.com>
2022-06-08 06:21:24 -0700

    Advanced Task 2

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,11 +3,11 @@
  * main - Entry point
  * Return:1
  */
-int main (void)
+int main(void)
 {
-	char str[]="and that piece of art is useful\" - Dora Korpar, 2015-10-19";
-	char *pStr=str;
-	
+	char str[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+	char *pStr = str;
+
 	fputs(pStr, stdout);
 	fputs("\n", stdout);
 	return (1);
