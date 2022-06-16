616cc27f88e6b690a99804c084534e66711a7af3
SimplicityND <ronaldomeh@yahoo.com>
2022-06-15 17:14:13 -0700

    Mandatory Task 3, Project 12

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -17,7 +17,7 @@ for (i = 0; i < 100; i++)
 			putchar(' ');
 			putchar((j / 10) + 48);
 			putchar((j % 10) + 48);
-			if (i != 98 || j != 99);
+			if (i != 98 || j != 99)
 			{
 				putchar(',');
 				putchar(' ');
