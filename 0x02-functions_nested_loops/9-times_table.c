1b88d6dff6245ded6f1f643631cf39f790a8d7c0
SimplicityND <ronaldomeh@yahoo.com>
2022-06-30 13:17:28 -0700

    Task 9

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,41 @@
+#include "main.h"
+
+/**
+ * times_table - prints times table
+ * Return:void
+ */
+
+void times_table(void)
+{
+
+int a = 0;
+int b;
+int rep;
+	for (a <= 9; b = 0; b <= 9)
+	{
+		rep = a * b;
+		if (b == -1)
+		{
+			_putchar('0' + rep);
+		}
+		else if (rep < 10)
+		{
+			_putchar(' ');
+			_putchar('0' + rep);
+		}
+		else
+		{
+			_putchar('0' + rep / 10);
+			_putchar('0' + rep % 10);
+		}
+
+		if (b < 9)
+		{
+			_putchar(',');
+			_putchar(' ');
+		}
+		b++;
+	}
+	_putchar('\n');
+	a++;
+}
