f4fb070b3af293707c8b320e41ce2f3dfe2c09bf
SimplicityND <ronaldomeh@yahoo.com>
2022-07-05 15:44:08 -0700

    10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - this function returns the sum of its parameters
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	int a;
+	int b;
+
+	int add = (a + b);
+	{
+	return (add);
+	}
+}
